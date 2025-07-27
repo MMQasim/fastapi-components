20283e35b728f40b7cae81fdcd2449f012afab10
ekko <muhammadmqasim01@gmail.com>
2025-07-27 15:29:07 +0200

    test for pre-commit

--- auth/fastapi_components_auth/auth_route.py
+++ auth/fastapi_components_auth/auth_route.py
@@ -1,24 +1,14 @@
 from fastapi import APIRouter, Depends
 
 
-router = APIRouter(tags=["auth"],prefix="/auth")
+router = APIRouter(tags=["auth"], prefix="/auth")
+
 
 @router.get("/validate")
 async def validate_auth():
     """
     Endpoint to validate authentication.
     This is a placeholder and should be replaced with actual validation logic.
-
-
-
     """
-
-
-
-
-
-
-
-
 
     return {"message": "Authentication is valid"}
